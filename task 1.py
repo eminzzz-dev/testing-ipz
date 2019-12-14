a3f36437438dbc89846cae12e652fd361375bbc5
eminzzz-dev <zosimovr@krok.edu.ua>
2019-12-14 21:33:11 +0200

    First Task

---
+++ task 1.py
@@ -0,0 +1,12 @@
+import cmath
+
+
+def main():
+    x = int(input("Введите x: "))
+    z1 = x ** 2 + 2 * x - 3 + (x + 1) * cmath.sqrt((x ** 2) - 9)
+    z2 = cmath.sqrt((x + 3) / (x - 3))
+    return f"Формула z1: {z1}\nФормула z2: {z2}"
+
+
+if __name__ == '__main__':
+    print(main())
